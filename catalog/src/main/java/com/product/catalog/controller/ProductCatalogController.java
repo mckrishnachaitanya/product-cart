2c35b973051811b383c48745833721e08dedc3f7
M C Krishna Chaitanya <krishnachaitanya.krish@gmail.com>
2024-04-24 22:43:05 +0530

    removed logger

--- catalog/src/main/java/com/product/catalog/controller/ProductCatalogController.java
+++ catalog/src/main/java/com/product/catalog/controller/ProductCatalogController.java
@@ -29,7 +29,6 @@ public class ProductCatalogController {
 
 	@PostMapping("/products")
 	public ResponseEntity<Product> createProduct(@RequestBody Product product) {
-		System.out.println(product.getId());
 		return ResponseEntity.ok().body(this.productService.createProduct(product));
 	}
 
